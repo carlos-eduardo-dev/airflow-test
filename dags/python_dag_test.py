737bb09e20bc8eb76b69c5188b386fc9bf1e5977
Carlos Eduardo <carlos-eduardo-dev@hotmail.com>
2025-04-06 21:54:15 -0300

    test

--- dags/python_dag_test.py
+++ dags/python_dag_test.py
@@ -3,6 +3,7 @@ from airflow.models import DAG
 from datetime import datetime, timedelta
 
 import time
+import requests
 from pprint import pprint
 
 seven_days_ago = datetime.combine(
@@ -20,9 +21,20 @@ dag = DAG(
 
 
 def print_context(ds, **kwargs):
-    pprint(kwargs)
-    print(ds)
-    return 'Whatever you return gets printed in the logs'
+    allPokemons = []
+    limit = 20
+    offset = 0
+    total = 1025
+    while offset <=total:
+        api_url = ("https://pokeapi.co/api/v2/pokemon-species/?limit=%s&offset=%s" % (limit, 20 * offset))
+        response_str = requests.get(api_url)
+        response_json = requests.get(api_url).json()
+        pokemons = []
+        [pokemons.append(pokemon['name']) for pokemon in response_json['results']]
+        print([pokemon for pokemon in pokemons])
+        allPokemons.extend(pokemons)            
+        offset = offset + 1
+    return allPokemons
 
 run_this = PythonOperator(
     task_id='print_the_context',
