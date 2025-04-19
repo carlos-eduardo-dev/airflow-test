5507af174b1b44b578695275359edd21737383c6
Carlos Eduardo <carlos-eduardo-dev@hotmail.com>
2025-04-18 21:13:48 -0300

    criação dag imdb

--- dags/imdb.py
+++ dags/imdb.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 
 from airflow.decorators import dag
 from airflow.models import Param
-from pendulum import datetime, today
+from pendulum import today
 
 DEFAULT_MINIO_CONNECTION_ID = "minio-connection"
 DEFAULT_MINIO_BUCKET_NAME = "imdb-raw"
@@ -16,10 +16,10 @@ DEFAULT_MINIO_BUCKET_NAME = "imdb-raw"
     schedule="@daily",
     default_args={"retries": 2, "retry_delay": timedelta(minutes=2)},
     params={
-        "arquivos_imdb": Param(
+        "datasets": Param(
             default=["title.basics.tsv.gz"],
             type="array",
-            title="Arquivos",
+            title="IMDb Datasets",
             description="Selecione um ou mais arquivos da IMDb Non-Commercial Datasets.",
             examples=[
                 "name.basics.tsv.gz",
@@ -31,13 +31,13 @@ DEFAULT_MINIO_BUCKET_NAME = "imdb-raw"
                 "title.ratings.tsv.gz",
             ],
             values_display={
-                "name.basics.tsv.gz": "Name basics",
+                "title.basics.tsv.gz": "Títulos",
                 "title.akas.tsv.gz": "Títulos Alternativos",
-                "title.basics.tsv.gz": "Título Principal",
-                "title.principals.tsv.gz": "Elenco",
-                "title.crew.tsv.gz": "Diretores/Escritores",
                 "title.episode.tsv.gz": "Episódios",
-                "title.ratings.tsv.gz": "Avaliações do Título",
+                "title.ratings.tsv.gz": "Avaliações",
+                "title.crew.tsv.gz": "Diretores/Escritores",
+                "title.principals.tsv.gz": "Elenco",
+                "name.basics.tsv.gz": "Dados Pessoais",
             },
         ),
     },
