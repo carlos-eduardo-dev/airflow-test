9d2f44da82f1687a2769e4e464533f6379ccf66e
Carlos Eduardo <carlos-eduardo-dev@hotmail.com>
2025-04-18 22:37:40 -0300

    criação dag imdb

--- dags/imdb.py
+++ dags/imdb.py
@@ -1,10 +1,16 @@
+import os
+import tempfile
 from datetime import timedelta
+from typing import List, Dict
+from urllib.parse import urljoin
 
+import requests
 from airflow.decorators import dag, task
+from airflow.exceptions import AirflowFailException
 from airflow.models import Param
 from airflow.models.param import ParamsDict
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from pendulum import today
-from typing import List
 
 DEFAULT_MINIO_CONNECTION_ID = "minio-connection"
 DEFAULT_MINIO_BUCKET_NAME = "imdb-raw"
@@ -19,11 +25,47 @@ DATASETS = {
 }
 
 
-@task
-def select_datasets(**kwargs) -> List[str]:
+@task(task_id="generate_urls", task_display_name="Generate URLs to download")
+def datasets_info(**kwargs) -> List[Dict[str, str]]:
     params: ParamsDict = kwargs["params"]
-    return params["datasets"]
+    url = params["url"]
+    datasets = params["datasets"]
+    return [{"name": dataset, "url": urljoin(url.rstrip("/") + "/", dataset)} for dataset in datasets]
 
+
+@task(task_id="download_dataset", task_display_name="Download dataset", retries=3, retry_delay=timedelta(minutes=2))
+def download_dataset(dataset: str) -> str:
+    dataset_name = dataset["name"]
+    dataset_url = dataset["url"]
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=dataset_name) as temp_file:
+            print(f"Baixando {dataset_name} para {temp_file.name}")
+            request = requests.get(dataset_url, stream=True, timeout=120)
+            request.raise_for_status()
+            for chunk in request.iter_content(chunk_size=1024 * 1024):
+                temp_file.write(chunk)
+
+        print(f"Download de {dataset_name} completo: {temp_file.name}")
+        return temp_file.name
+    except requests.exceptions.RequestException as e:
+        print(f"Erro no download de {dataset_name}: {e}")
+        raise AirflowFailException(f"Falha ao baixar {dataset_name} de {dataset_url}")
+
+@task(task_id="create_bucket", task_display_name="Create bucket if not exists")
+def create_bucket():
+    s3_hook = S3Hook(aws_conn_id=DEFAULT_MINIO_CONNECTION_ID)
+    s3_bucket = DEFAULT_MINIO_BUCKET_NAME
+    if not s3_hook.check_for_bucket(bucket_name=s3_bucket):
+        s3_hook.create_bucket(bucket_name=s3_bucket)
+
+
+@task(task_id="delete_temp_file", task_display_name="Delete temporary file")
+def delete_temp_file(path):
+    if os.path.exists(path):
+        os.remove(path)
+        print(f"Arquivo {path} removido")
+    else:
+        print(f"Arquivo {path} não encontrado")
 
 @dag(
     dag_id="imdb",
@@ -50,7 +92,7 @@ def select_datasets(**kwargs) -> List[str]:
     tags=['imdb'],
 )
 def dag():
-    select_datasets()
-
-
+    datasets = datasets_info()
+    downloaded_datasets = download_dataset.expand(dataset=datasets)
+    datasets_info() >> [downloaded_datasets]
 dag()
